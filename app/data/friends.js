cb0a5f4e989bb22e396359ff04011d9de5f40cde
Elisabeth Edrington <edringtondc@gmail.com>
2018-12-18 12:02:37 -0700

    initial file add

---
+++ app/data/friends.js
@@ -0,0 +1,106 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. 
+//Each of these objects should roughly follow the format below.
+
+// TO DO: 
+//     PHOTOS
+//     SCORES based on questions?
+
+var friends = [
+    {
+        "name": "Ross",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+    , {
+        "name": "Rachel",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            1,
+            5,
+            4,
+            2,
+            5,
+            1,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Chandler",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Joey",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Monica",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    }, {
+        "name": "Phoebe",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    }
+
+
+]
---
+++ app/server.js
@@ -0,0 +1,5 @@
+//Your server.js file should require the basic npm packages we've used in class: express and path.
+var express = require("express");
+var path = require("path");
+
+var questions = [""]
