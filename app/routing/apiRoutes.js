84832c34601dcb5c78ad3965d28cba839dce7a81
Elisabeth Edrington <edringtondc@gmail.com>
2018-12-19 20:23:21 -0700

    added style sheet, api routes and html routes currently working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,53 @@
 // Your apiRoutes.js file should contain two routes:
 
-    // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-    // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var friends = require("../data/friends.js")
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+module.exports = function(app) {
+
+  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  
+// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+//   app.post("/api/friends", function(req, res) {
+
+
+ 
+
+//   });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+//   app.post("/api/clear", function(req, res) {
+//     // Empty out the arrays of data
+//     tableData.length = [];
+//     waitListData.length = [];
+
+//     res.json({ ok: true });
+//   });
+};
--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
+//===========================================================================================
 // Dependencies
 // =============================================================
 var express = require("express");
 var path = require("path");
+var $ = require('jQuery')
 
 // Sets up the Express App
 // =============================================================
@@ -13,5 +15,20 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
